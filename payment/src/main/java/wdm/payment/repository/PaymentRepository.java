5acf2aa470ca92d7676b7165d6296fca5f08ff9b
Serenic <serenic@monte.nl>
2023-06-02 18:54:41 +0200

    Locking

--- payment/src/main/java/wdm/payment/repository/PaymentRepository.java
+++ payment/src/main/java/wdm/payment/repository/PaymentRepository.java
@@ -1,5 +1,7 @@
 package wdm.payment.repository;
 
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import wdm.payment.model.Payment;
 
@@ -7,6 +9,7 @@ import java.util.Optional;
 
 public interface PaymentRepository extends CrudRepository<Payment, Long> {
 
+    @Lock(LockModeType.OPTIMISTIC)
     public Optional<Payment> findByUserIdAndOrderId(long userId, long orderId);
     public boolean existsByUserIdAndOrderId(long user_id, long order_id);
 }
--- payment/src/main/java/wdm/payment/repository/UserRepository.java
+++ payment/src/main/java/wdm/payment/repository/UserRepository.java
@@ -1,8 +1,15 @@
 package wdm.payment.repository;
 
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import wdm.payment.model.User;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {}
+public interface UserRepository extends CrudRepository<User, Long> {
+    @Lock(LockModeType.OPTIMISTIC)
+    Optional<User> findById(Long aLong);
+}
