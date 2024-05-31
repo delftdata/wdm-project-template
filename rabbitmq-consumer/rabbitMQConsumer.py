ce615c2e2822800a7221c6aad28352e2a3105798
mennohielkema <M.Hielkema-1@student.tudelft.nl>
2024-05-31 16:55:30 +0200

    Add retries when services crash

--- rabbitmq-consumer/rabbitMQConsumer.py
+++ rabbitmq-consumer/rabbitMQConsumer.py
@@ -8,8 +8,9 @@ class RabbitMQConsumer:
     def process(message: dict, functions: dict):
         functions.update(globals())
         if 'function' not in message.keys():
-            return
-        functions[message['function']](*message['args'])
+            # Invalid request, do not retry
+            return True
+        return functions[message['function']](*message['args'])
 
     def consume_queue(self, queue: str, functions: dict):
         while True:
@@ -22,13 +23,37 @@ class RabbitMQConsumer:
                 print(f"Failed to connect to RabbitMQ: {str(e)}")
                 time.sleep(5)
             else:
+                print(f"RabbitMQ Connected")
                 break
         channel = conn.channel()
         channel.queue_declare(queue=queue, durable=True)
-        for method, properties, body in channel.consume(queue=queue, inactivity_timeout=30):
-            if body:
-                res = json.loads(body.decode())
-                self.process(res, functions)
-
-                # We signal that the message is received and processed, rabbitMQ will now remove it from the queue
-                channel.basic_ack(delivery_tag=method.delivery_tag)
+        while True:
+            try:
+                for method, properties, body in channel.consume(queue=queue, inactivity_timeout=30):
+                    if body:
+                        res = json.loads(body.decode())
+                        response = self.process(res, functions)
+                        # We signal that the message is received and processed, rabbitMQ will now remove it from the
+                        # queue
+                        if response:
+                            channel.basic_ack(delivery_tag=method.delivery_tag)
+                        else:
+                            # Try again later
+                            channel.basic_nack(delivery_tag=method.delivery_tag)
+            except (pika.exceptions.StreamLostError, pika.exceptions.ConnectionClosedByBroker):
+                print("Connection to RabbitMQ Lost. Retrying connection...")
+                while True:
+                    try:
+                        conn = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+                    except pika.exceptions.AMQPConnectionError:
+                        print(f"Retrying connection for queue {queue}...")
+                        time.sleep(5)
+                    except Exception as e:
+                        print(f"Failed to connect to RabbitMQ: {str(e)}")
+                        time.sleep(5)
+                    else:
+                        print(f"RabbitMQ Connected")
+                        break
+                    time.sleep(3)
+                channel = conn.channel()
+                channel.queue_declare(queue=queue, durable=True)
